2e9fa1872d631b9e1860b5fafc556cd764fc07c6
rodriquelali <rodriquelali@gmail.com>
2026-05-21 22:03:59 -0400

    Examplo with the clase R.

---
+++ MyApplicationCode/app/src/main/java/com/example/myapplicationcode/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplicationcode
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        //empiezo hacer codigo
+
+        //conectar a mi boton y caja de texto
+        // de la clase R
+        //casting
+        val btMensaje = findViewById<Button>(R.id.btnMensaje)
+
+        //crando lavariable especifico
+        val txtMen: EditText = findViewById(R.id.txtMensaje)
+
+
+
+    }
+}
