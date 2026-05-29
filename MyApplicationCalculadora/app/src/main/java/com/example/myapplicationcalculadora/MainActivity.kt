f84d12b47e242edb6b963a2358a27d02e5164c4d
rodriquelali <rodriquelali@gmail.com>
2026-05-28 22:01:49 -0400

    Calculadora parte 1.

--- MyApplicationCalculadora/app/src/main/java/com/example/myapplicationcalculadora/MainActivity.kt
+++ MyApplicationCalculadora/app/src/main/java/com/example/myapplicationcalculadora/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,41 +22,66 @@ class MainActivity : AppCompatActivity() {
         }
 
         //conectar con clase R
-        val txtN1 = findViewById<EditText>(R.id.txtNum1)
-        val txtN2 = findViewById<EditText>(R.id.txtNum2)
+        val txtN1 = findViewById<TextInputEditText>(R.id.txtValorOperar)
+        val tvView = findViewById<TextView>(R.id.tvView)
+        val tvResul = findViewById<TextView>(R.id.tvResultado)
+
 
         val btnSuma = findViewById<Button>(R.id.btnSuma)
         val btnResta = findViewById<Button>(R.id.btnResta)
         val btnMulti = findViewById<Button>(R.id.btnMultiplicacion)
         val btnDivision = findViewById<Button>(R.id.btnDivision)
+        val btnIgual = findViewById<Button>(R.id.btnIgual)
 
-        val tvResul = findViewById<TextView>(R.id.tvResultado)
+
+        //numeros
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn9 = findViewById<Button>(R.id.btn9)
+        val btn0 = findViewById<Button>(R.id.btn0)
+        val btnComa = findViewById<Button>(R.id.btn)
 
         //programacion estructura
         //POO
         //Procxima clas es POO
         btnSuma.setOnClickListener {
-            val num1 = txtN1.text.toString().toInt()
+            /*val num1 = txtN1.text.toString().toInt()
             val num2 = txtN2.text.toString().toInt()
             val resul: Int = num1 + num2
-            tvResul.text = resul.toString()
+            tvResul.text = resul.toString()*/
         }
 
         btnResta.setOnClickListener {
-            val num1 = txtN1.text.toString().toInt()
+            /*val num1 = txtN1.text.toString().toInt()
             val num2 = txtN2.text.toString().toInt()
             val resul: Int = num1 - num2
-            tvResul.text = resul.toString()
+            tvResul.text = resul.toString()*/
         }
 
         btnMulti.setOnClickListener {
-            val num1 = txtN1.text.toString().toInt()
+            /*val num1 = txtN1.text.toString().toInt()
             val num2 = txtN2.text.toString().toInt()
             val resul: Int = num1 * num2
-            tvResul.text = resul.toString()
+            tvResul.text = resul.toString()*/
         }
 
         //division, analizar division
+        btnDivision.setOnClickListener {
+            /*val num1 = txtN1.text.toString().toInt()
+            val num2 = txtN2.text.toString().toInt()
+            if(num2 == 0){
+                  tvResul.text = "No se puede dividir entre 0"
+            }else{
+                val resul: Int = num1 / num2
+                tvResul.text = resul.toString()
+            }*/
+        }
 
     }
     //funciones
