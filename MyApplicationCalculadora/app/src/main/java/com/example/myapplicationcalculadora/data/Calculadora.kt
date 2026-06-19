c04bf3c68f4ef840135dad8c1e36342c8abd0266
rodriquelali <rodriquelali@gmail.com>
2026-06-18 20:58:11 -0400

    Documentacion kotlin.

--- MyApplicationCalculadora/app/src/main/java/com/example/myapplicationcalculadora/data/Calculadora.kt
+++ MyApplicationCalculadora/app/src/main/java/com/example/myapplicationcalculadora/data/Calculadora.kt
@@ -1,11 +1,27 @@
 package com.example.myapplicationcalculadora.data
 
+//documentacion
+
+
+/**
+ * Calculadora, recibe 2 numeros, realiza las operaciones en formato
+ * LATAM
+ * @param numero1 primer numero
+ * @param numero2 segundo numero
+ * @version 1.0
+ * @author Rodrigo
+ * */
 class Calculadora(
     private val numero1: String,
     private val numero2: String,
     private var formatoLatam: FormatoLatam = FormatoLatam())  {
 
     //analizar que pas si yo paso los valores de 2,4 + 4,5 = 6,9
+
+    /**
+     * Suma de 2 numeros, recibe 2 numeros en formato LATAM
+     * @return resultado de la suma, en formato String
+     * */
     fun suma():String{
         var n1 = formatoLatam.validadorComa(numero1)
         var n2 = formatoLatam.validadorComa(numero2)
